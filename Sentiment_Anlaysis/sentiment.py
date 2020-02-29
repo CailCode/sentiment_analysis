944964c3b7ad1855823a84a8eb522b1a49a4707d
Caleb Gucciardi <calebgucciardi@gmail.com>
2020-02-29 11:36:03 +0100

    Update sentiment.py

--- Sentiment_Anlaysis/sentiment.py
+++ Sentiment_Anlaysis/sentiment.py
@@ -4,40 +4,43 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.metrics import accuracy_score
-from sklearn.tree import DecisionTreeClassifier
 
+#dataset
 df = pd.read_csv("movie.csv")
 
-X = df['text']
-y= df['tag']
+X = df['text'] #features
+y= df['tag'] #target
 
 vectorizer = CountVectorizer()
 X = vectorizer.fit_transform(X)
+# let's vectorize the string with Bag of Words method
 
-
+# split the dataset in train and test
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
 
 print("--Bernoulli--")
 model = BernoulliNB()
-model.fit(X_train,y_train)
+model.fit(X_train,y_train) # train the model
 
 train_predict = model.predict(X_train)
 test_predict = model.predict(X_test)
 
+# 
 acc_tr = accuracy_score(y_train,train_predict)
 acc_te = accuracy_score(y_test,test_predict)
-
+#
 print(f"ACC TRAIN::{acc_tr} -- ACC TEST:{acc_te}")
+# accuracy of test is 70%
 
-# inizia il vero programma
+# start 
 while True:
-    print("---Scrivi la tua recensione!---")
+    print("---Write a Review!---")
     review = []
     review.append(input())
     review = vectorizer.transform(review)
     print(f"La tua recensione è::{model.predict(review)}")
-    esc = input("Vuoi uscire::")
-    if esc=="si": break
+    esc = input("Exit::yes-no::")
+    if esc=="yes": break
     
 
 
